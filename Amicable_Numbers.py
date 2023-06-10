14a61809c9335138f9861daf93d7a684639757bd
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2023-06-10 22:13:52 +0530

    n=int(input()) m=int(input()) def fac(n):     su=0     for i in range(1,n):         if n%i==0:             su+=i     return su if fac(n)==m and fac(m)==n:     print('Amicable') else:     print('Not Amicable')

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,21 +1,12 @@
-import math
 n=int(input())
 m=int(input())
-sum1=0
-sum2=0
-for i in range(1,int(math.sqrt(n))+1):
-    if(n%i==0):
-        sum1+=i
-        #print(i,end=" ")
-        if(n/i!=i):
-            sum1+=n/i
-         #   print(n/i,end=" ")
-for j in range(1,int(math.sqrt(m))+1):
-    if(m%j==0):
-        sum2+=j
-        if(m/j!=j):
-            sum2+=m/j
-if(sum1-n==m and sum2-m==n):
-    print("Amicable")
+def fac(n):
+    su=0
+    for i in range(1,n):
+        if n%i==0:
+            su+=i
+    return su    
+if fac(n)==m and fac(m)==n:
+    print('Amicable')
 else:
-    print("Not Amicable")
+    print('Not Amicable')
