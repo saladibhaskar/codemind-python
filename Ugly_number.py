a2ca6cff5f9ec33c357800f921b10c1d0132c632
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2023-06-15 10:16:06 +0530

    n=int(input()) def isugly(n):     if n==1:         return True     elif n%2==0:         return isugly(n//2)     elif n%3==0:         return isugly(n//3)     elif n%5==0:         return isugly(n//5)     return False if (isugly(n)):     print("Ugly Number") else:     print("Not Ugly Number")

---
+++ Ugly_number.py
@@ -0,0 +1,15 @@
+n=int(input())
+def isugly(n):
+    if n==1:
+        return True
+    elif n%2==0:
+        return isugly(n//2)
+    elif n%3==0:
+        return isugly(n//3)
+    elif n%5==0:
+        return isugly(n//5)
+    return False
+if (isugly(n)):
+    print("Ugly Number")
+else:
+    print("Not Ugly Number")
